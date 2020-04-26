4885bb68cb3c2f2b3635b5aee06c11364fff8c0b
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-26 23:02:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  depth_history = []
+  view = data
+  
+  # drill down until you reach value that cannot be nested
+  while view.class == Array || Hash do
+    
+    if view.class == Hash
+      depth_history << view.first[0]
+      view = view.first[1]
+    else
+      # depth_history << view.first
+      view = view.first
+    end
+    
+  end
+  binding.pry
+
 end
